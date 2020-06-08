c2946a6ecb36fb3e9a5d591484238ec7a92c08c4
Sidélia Silva <sidelialuiza@gmail.com>
2020-06-08 15:28:59 -0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,9 @@
+def square(x):
+	return x * x
+
+def main()
+	for i in range(10):
+		print("{} squared is {}".format(i, square(i)))
+
+if _name_ == "_main_"
+	main()
---
+++ sets.py
@@ -0,0 +1,6 @@
+s = sets()
+s.add(1)
+s.add(3)
+s.add(5)
+s.add(3)
+print(s)
